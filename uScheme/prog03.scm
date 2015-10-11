92f19ce7083d95758d966653b5d483fee4b0ddcb
Jesse Jurman <j.r.jurman@gmail.com>
2015-10-10 22:09:11 -0400

    attempt median

--- uScheme/prog03.scm
+++ uScheme/prog03.scm
@@ -43,3 +43,53 @@
 (define minValue (xs)
   (minStarter xs 0)
 )
+
+(define avgHelper (xs avg avgCount)
+  (if (null? xs) (/ avg avgCount)
+    (avgHelper (cdr xs) (+ (car xs) avg) (+ avgCount 1))
+  )
+)
+
+(define avgStarter (xs default)
+  (if (null? xs) default
+    (avgHelper xs 0 0)
+  )
+)
+
+(define avgValue (xs)
+  (avgStarter xs 0)
+)
+
+(define medMinSearch (xs medMin medMax medMinXs medMaxXs tmpMinXs tmpMaxXs)
+  (if (null? xs) medMin
+    (if (> (car xs) medMin)
+      (if (findInList (car xs) (tmpMinXs))
+        (medMinSearch (cdr xs) medMin medMax minMedXs medMaxXs (cdr tmpMinXs) tmpMaxXs)
+        (medMaxSearch (cdr xs) medMin medMax minMedXs medMaxXs (cons medMinXs (car xs)) medMaxXs)
+      )
+      (medMinSearch (cdr xs) medMin medMax medMinXs medMaxXs medMinXs medMaxXs)
+    )
+  )
+)
+
+(define medMaxSearch (xs medMin medMax medMinXs medMaxXs tmpMinXs tmpMaxXs)
+  (if (null? xs) medMax
+    (if (< (car xs) medMax)
+      (if (findInList (car xs) (tmpMaxXs))
+        (medMaxSearch (cdr xs) medMin medMax minMedXs medMaxXs tmpMinXs (cdr tmpMaxXs))
+        (medMinSearch (cdr xs) medMin medMax minMedXs medMaxXs medMinxs (cons medMaxXs (car xs)))
+      )
+      (medMaxSearch (cdr xs) medMin medMax medMinXs medMaxXs medMinXs medMaxXs)
+    )
+  )
+)
+
+(define medStarter (xs default)
+  (if (null? xs) default
+    (medMinSearch xs (minValue xs) (maxValue xs) (minValue xs) (maxValue xs) '(minValue xs) '(maxValue xs) '(minValue xs) '(maxValue xs))
+  )
+)
+
+(define medValue (xs)
+  (medStarter xs 0)
+)
